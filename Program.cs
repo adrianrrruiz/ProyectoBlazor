f6cdcc0a35c85ec463b6229b34841210520bb0ac
Adrian Ruiz <adrianruiz0208@gmail.com>
2023-09-20 22:01:02 -0500

    Añadiendo comentarios de conexión a API

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ using ProyectoBlazor;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+//Esta linea nos permite obtener un valor del archivo de configuracion appsettings
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+//Http Client es la clase que se encarga de hacer conexiones con APIs y servicios externos
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
